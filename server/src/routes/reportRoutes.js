fb912d5737536cd54297a9ac5482bd51f0a088cd
shireihuy <125983081+shireihuy@users.noreply.github.com>
2026-02-06 11:51:08 +0700

    Fix case-sensitive role authorization in report routes

--- server/src/routes/reportRoutes.js
+++ server/src/routes/reportRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { getReportData } = require('../controllers/reportController');
 const { authenticateToken, authorizeRoles } = require('../middleware/auth');
 
-router.get('/data', authenticateToken, authorizeRoles('admin', 'manager'), getReportData);
+router.get('/data', authenticateToken, authorizeRoles('Admin', 'Manager'), getReportData);
 
 module.exports = router;
